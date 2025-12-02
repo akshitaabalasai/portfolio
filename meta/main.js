4bfca497fdda8a4cd7012d2fa40f90e04b92a7ce
balatechnet <balasai.gopalakrishnan@smud.org>
2025-12-02 13:32:06 -0800

    changes

--- meta/main.js
+++ meta/main.js
@@ -1,7 +1,6 @@
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 import scrollama from "https://cdn.jsdelivr.net/npm/scrollama@3.2.0/+esm";
 
-/* ---------- Parsers & helpers ---------- */
 const fmtInt = d3.format(",");
 const parseDate = d3.timeParse("%Y-%m-%d");
 const parseTimeHMS = d3.timeParse("%H:%M:%S");
